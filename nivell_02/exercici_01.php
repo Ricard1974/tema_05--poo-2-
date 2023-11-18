e022b73d01fca196860513f0339cdec01a2e3868
ricardPortatil <ricardmagnet@gmail.com>
2023-11-18 23:22:51 +0100

    petites correccions de texte

--- nivell_02/exercici_01.php
+++ nivell_02/exercici_01.php
@@ -8,14 +8,14 @@ class Forma implements FormaInterface// seria  la clase pare y usa el contrato
 {
 
     public $amplada; // tambe es poden posar en protected, private
-    public $alcada;
+    public $alçada;
     public $texte;
 
 
-    public function __construct($amplada, $alcada, $texte)
+    public function __construct($amplada, $alçada, $texte)
     {
         $this->amplada = $amplada;
-        $this->alcada = $alcada;
+        $this->alçada = $alçada;
         $this->texte=$texte;
 
     }
@@ -30,11 +30,11 @@ class Rectangle extends Forma  // la clase fill hereda (area), obligado a usar e
 {
     public function area(){
         echo '<br/>';
-        echo 'Amb una base de '.$this->amplada.' y una alçada de '.$this->alcada;
+        echo 'Amb una base de '.$this->amplada.' y una alçada de '.$this->alçada;
         echo '<br/>';
         echo $this->texte.'<br/>';
 
-        return $this->amplada * $this-> alcada;
+        return $this->amplada * $this-> alçada;
     }
 }
 class Triangle extends Forma // clase fill que hereda area()
@@ -42,10 +42,10 @@ class Triangle extends Forma // clase fill que hereda area()
     public function area(){
 
         echo '<br/>';
-        echo 'Amb una base de '.$this->amplada.' y una alçada de '.$this->alcada;
+        echo 'Amb una base de '.$this->amplada.' y una alçada de '.$this->alçada;
         echo '<br/>';
         echo $this->texte.'<br/>';
-        return ($this->amplada * $this-> alcada)/2;
+        return ($this->amplada * $this-> alçada)/2;
     }
 }
 
@@ -55,6 +55,6 @@ echo '<br/>';
 echo 'El area del rectangle es de '.$rectangle->area();
 
 
-$triangle= new Triangle(5,4,''); // amplada , alcada i missatge
+$triangle= new Triangle(5,4,''); // amplada , alçada i missatge
 echo '<br/>';
 echo 'El area del triangle es de '.$triangle->area();
